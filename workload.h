44d3c64d76aefbd513d2f7af5ca9e6035fd3a3cc
Rita2004 <129674873+Rita2004@users.noreply.github.com>
2024-05-08 19:29:16 +0400

    Update workload.h

--- workload.h
+++ workload.h
@@ -1,1 +1,9 @@
+#ifndef WORKLOAD_H
+#define WORKLOAD_H
+
+#include "scheduler.h"  // For the workload_item struct
+
+int load_workload(const char *filename, workload_item **workload_out, size_t *workload_size_out);
+
+#endif
 
