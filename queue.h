fd2b46ab34825bce88dbf59b36de9af88317d060
Rita2004 <129674873+Rita2004@users.noreply.github.com>
2024-05-08 19:17:09 +0400

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,23 +1,19 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-#include "scheduler.h" // Include the workload_item type 
+#include "scheduler.h" 
 
-// Node for our queue implementation 
 typedef struct node_t {
     int prio; 
     int pid; 
     struct node_t *next;
 } node_t;
 
-// Could be implemented with more advanced techniques 
-// if performance is critical
-
-// Queue operations
 node_t *create_node(int prio, int pid);
 void enqueue(node_t **queue, int prio, int pid);
-int dequeue(node_t **queue);  // Gets (and removes) the head element's pid 
-int peek(node_t *queue); // Gets the head element's pid without removing
+int dequeue(node_t **queue);  
+int peek(node_t *queue); 
 int is_empty(node_t *queue); 
+char* queue_to_string(node_t* queue); 
 
-#endif // QUEUE_H 
+#endif 
